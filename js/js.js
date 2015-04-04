ac706cfb8b02e800c86525a8b175ec2e89997213
Stickman <stocktonmanners@yahoo.ca>
2015-04-04 00:28:05 -0600

    static pic

---
+++ js/js.js
@@ -0,0 +1,9 @@
+// you can enter your JS here!
+
+/*
+so what they are doing is loading the image
+then preloading the next image and then fading (opacity to 0) it in to the next image
+also they are stashing the picture at the bottom of the page and hiding for quick reload.
+
+Don't know if I need the pic counter
+*/
