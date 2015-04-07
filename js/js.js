213d4fc89dc7b3ced998c80caf4c25b26fdaf93a
Stickman <stocktonmanners@yahoo.ca>
2015-04-07 02:10:46 -0600

    better

--- js/js.js
+++ js/js.js
@@ -1,33 +1,22 @@
 // you can enter your JS here!
 
-/*
-so what they are doing is loading the image
-then preloading the next image and then fading (opacity to 0) it in to the next image
-also they are stashing the picture at the bottom of the page and hiding for quick reload.
-
-Don't know if I need the pic counter
-
-*/
-
 
 (function($) {
 
-// #load variables
 /* the map */
 // #get hotel name for map creation
 var hotel_encoded = $('h1.hotel_name').clone().children().remove().end().text();
-// a real hotel for demo
-hotel_encoded = 'Banff Boutique Inn - Pension Tannenhof'
+// use a real hotel for demo
+hotel_encoded = 'Banff Boutique Inn - Pension Tannenhof';
 hotel_encoded = encodeURIComponent(hotel_encoded);
-// console.log(hotel_encoded);
 
+// map html
 var mappie = '<iframe width="300" height="200" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=' + hotel_encoded + '&key=AIzaSyD1Z5-p_E_ls9hwu__Ie84MwtSB_8ha8xk"></iframe>';
 $('.map').html('');
 $('.map').append(mappie);
 
-/* the fader */
 
-
+/* the carousel */
 var list = new Array();
 var increment = 0;
 var timer = null;
@@ -44,7 +33,6 @@ $.when(
         rotate()
   });
 });
-
 
 
 function rotate(){
@@ -73,23 +61,19 @@ function rotate(){
 }
 
 var photos_d = $('#photos_distinct ul li a');
+
+// show full size photo when hover on thumbnail and stop photo rotation
 photos_d.on('mouseover', function () {
-      var t = $(this).attr('href');
+  var t = $(this).attr('href');
 
-    window.clearTimeout(timer)
-$("#active").attr('src', t);
-
-
-console.log('stop');
+    window.clearTimeout(timer);
+  $("#active").attr('src', t);
 });
 
-
+// recommence photo rotation
 photos_d.on('mouseout', function () {
     updatetimer();
-    console.log('start');
 });
-
-
 
 
 }(jQuery));
